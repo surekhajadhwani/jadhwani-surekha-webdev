2b57dad2025f2f309a0718c2c49e6162ebabcfae
surekha <surekha@ccs.neu.edu>
2016-09-18 11:32:15 -0700

    Updated for Heroku

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+console.log(process.env);
 
 app.listen(3000);
