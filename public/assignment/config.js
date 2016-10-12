aa9b8b6b5ec97a1cc8113591975e87a32f64f2c4
surekha <surekha@ccs.neu.edu>
2016-10-11 22:23:07 -0700

    Configured initial angular app, views and routing

---
+++ public/assignment/config.js
@@ -0,0 +1,50 @@
+(function() {
+    angular
+        .module("WebAppMaker")
+        .config(Config);
+
+    function Config($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "views/user/login.view.client.html"
+                //,controller: "LoginController"
+            })
+            .when("/login", {
+                templateUrl: "views/user/login.view.client.html"
+                //,controller: "LoginController"
+            })
+            .when("/register", {
+                templateUrl: "views/user/register.view.client.html"
+            })
+            .when("/user/:uid", {
+                templateUrl: "views/user/profile.view.client.html"
+            })
+            .when("/user/:uid/website", {
+                templateUrl: "views/website/website-list.view.client.html"
+            })
+            .when("/user/:uid/website/new", {
+                templateUrl: "views/website/website-new.view.client.html"
+            })
+            .when("/user/:uid/website/:wid", {
+                templateUrl: "views/website/website-edit.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page", {
+                templateUrl: "views/pages/page-list.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/new", {
+                templateUrl: "views/pages/pages-new.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid", {
+                templateUrl: "views/pages/page-edit.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget", {
+                templateUrl: "views/widgets/widget-list.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget/new", {
+                templateUrl: "views/widgets/widget-chooser.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget/:wgid", {
+                templateUrl: "views/widgets/widget-edit.view.client.html"
+            });
+    }
+})();
