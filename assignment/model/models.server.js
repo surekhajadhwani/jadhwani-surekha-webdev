aa1f0a76ec4a0bf4814edc833b16084363155a00
surekha <surekha@ccs.neu.edu>
2016-11-28 22:12:55 -0800

    Updated connection string for heroku

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -2,17 +2,9 @@ module.exports = function() {
 
     var connectionString = 'mongodb://heroku_0v1rk2bt:l3t32hhnf85788sr9k54rmuu89@ds033116.mlab.com:33116/heroku_0v1rk2bt';
 
-    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-            process.env.OPENSHIFT_APP_NAME;
-    }
-
     var mongoose = require("mongoose");
-    // mongoose.connect(connectionString);
-    mongoose.connect("mongodb://localhost/cs5610-assignment");
+    mongoose.connect(connectionString);
+    // mongoose.connect("mongodb://localhost/cs5610-assignment");
 
     var userModel = require("./user/user.model.server")();
     var websiteModel = require("./website/website.model.server")();
