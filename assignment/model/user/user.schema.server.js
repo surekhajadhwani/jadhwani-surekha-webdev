d721dfe6144ed7cc17daa7cfb0e60fff5bcc7e28
surekha <surekha@ccs.neu.edu>
2016-12-12 00:35:31 -0800

    schema update

--- assignment/model/user/user.schema.server.js
+++ assignment/model/user/user.schema.server.js
@@ -2,7 +2,7 @@ module.exports = function () {
     var mongoose = require("mongoose");
     var UserSchema = mongoose.Schema({
         username: {type: String, required: true},
-        password: {type: String, required: true},
+        password: String,
         facebook:   {
             id:    String,
             token: String
